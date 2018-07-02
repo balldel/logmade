9122268b3156a8d5ba4a304f147edae6f6c239b7
Dell NC <balldel@gmail.com>
2018-07-02 10:04:40 +0700

    NC

--- createlog.py
+++ createlog.py
@@ -1,11 +1,16 @@
 import csv
+import os, sys
 from datetime import datetime
 from datetime import timedelta
 
 def dmk() :
-    place = "DMK_S1"
+    place = "CNX_WC_S1-6"
     month = "05"
-    lastdateofmonth = 2
+    lastdateofmonth = 30
+
+    dirpath = os.getcwd()
+    path = dirpath+"/"+place+"/"
+    os.makedirs( path, 755 )
     
     filename = []
     with open('test.csv', 'r') as k:
@@ -16,10 +21,10 @@ def dmk() :
 
     for x in range(1,lastdateofmonth+1):
         x = str(x)
-        startdatetime = datetime.strptime( x+"/"+month+"/06 - 00:00:00", "%d/%m/%y - %H:%M:%S")
-        stopdatetime = datetime.strptime( x+"/"+month+"/06 - 23:59:59", "%d/%m/%y - %H:%M:%S")
+        startdatetime = datetime.strptime( "2018/"+month+"/"+x+" - 00:00:00", "%Y/%m/%d - %H:%M:%S")
+        stopdatetime = datetime.strptime( "2018/"+month+"/"+x+" - 23:59:59", "%Y/%m/%d - %H:%M:%S")
         
-        with open(x+"_"+month+"_"+place+".csv", "w", newline="") as f:
+        with open(path+x+"_"+month+"_"+place+".csv", "w", newline="") as f:
             fw = csv.writer(f)
             fw.writerow(["Type","Start time","End time","Content Type","Content Name"])
 
@@ -28,8 +33,8 @@ def dmk() :
                 for m in filename:
                     enddatetime = startdatetime + timedelta(seconds=m['duration'])
 
-                    sdatetime = datetime.strftime(startdatetime, "%d/%m/%y - %H:%M:%S" )
-                    edatetime = datetime.strftime(enddatetime, "%d/%m/%y - %H:%M:%S" )
+                    sdatetime = datetime.strftime(startdatetime, "%Y/%m/%d - %H:%M:%S" )
+                    edatetime = datetime.strftime(enddatetime, "%Y/%m/%d - %H:%M:%S" )
                     
                     fw.writerow(["Z=Video","S="+sdatetime,"e="+edatetime,"I=video","N="+place+"/"+m['file']+".mp4"])
 
