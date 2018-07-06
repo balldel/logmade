ac9e647d62b830686ebcc55c902e97516e0d79b8
Dell MBP <balldel@gmail.com>
2018-07-06 13:40:16 +0700

    MBP

--- createlog.py
+++ createlog.py
@@ -4,13 +4,13 @@ from datetime import datetime
 from datetime import timedelta
 
 def dmk() :
-    place = "CNX_WC_S1-6"
+    place = "DMK_DIR55_S1-6"
     month = "05"
-    lastdateofmonth = 30
+    lastdateofmonth = 31
 
     dirpath = os.getcwd()
     path = dirpath+"/"+place+"/"
-    os.makedirs( path, 755 )
+    os.makedirs( path, 493 )
     
     filename = []
     with open('test.csv', 'r') as k:
@@ -21,8 +21,8 @@ def dmk() :
 
     for x in range(1,lastdateofmonth+1):
         x = str(x)
-        startdatetime = datetime.strptime( "2018/"+month+"/"+x+" - 00:00:00", "%Y/%m/%d - %H:%M:%S")
-        stopdatetime = datetime.strptime( "2018/"+month+"/"+x+" - 23:59:59", "%Y/%m/%d - %H:%M:%S")
+        startdatetime = datetime.strptime( "2018/"+month+"/"+x+" 00:00:00", "%Y/%m/%d - %H:%M:%S")
+        stopdatetime = datetime.strptime( "2018/"+month+"/"+x+" 23:59:59", "%Y/%m/%d - %H:%M:%S")
         
         with open(path+x+"_"+month+"_"+place+".csv", "w", newline="") as f:
             fw = csv.writer(f)
@@ -35,8 +35,9 @@ def dmk() :
 
                     sdatetime = datetime.strftime(startdatetime, "%Y/%m/%d - %H:%M:%S" )
                     edatetime = datetime.strftime(enddatetime, "%Y/%m/%d - %H:%M:%S" )
+                    playtime = str(m['duration'])
                     
-                    fw.writerow(["Z=Video","S="+sdatetime,"e="+edatetime,"I=video","N="+place+"/"+m['file']+".mp4"])
+                    fw.writerow(["Z=Video","S="+sdatetime,"e="+edatetime,"I=video","N="+m['file']+"_V1-"+playtime+"s."+m['file']+".mp4"])
 
                     startdatetime = enddatetime             
 
