d9194cb8a64d6a97f288a2c3b279ce643278462e
Dell MBP <balldel@gmail.com>
2018-07-12 15:48:20 +0700

    MBP

--- createlog.py
+++ createlog.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from datetime import timedelta
 
 def dmk() :
-    place = "DMK_DIR55_S1-6"
-    month = "05"
-    lastdateofmonth = 31
+    place = "CEI_T2_UP"
+    month = "04"
+    lastdateofmonth = 30
 
     dirpath = os.getcwd()
     path = dirpath+"/"+place+"/"
@@ -16,28 +16,28 @@ def dmk() :
     with open('test.csv', 'r') as k:
         reader = csv.reader(k)
         for row in reader:
-            filename.append({'file': row[0], 'duration': int(row[1])})
+            filename.append({'no': row[0] ,'file': row[1], 'duration': int(row[2])})
 
 
     for x in range(1,lastdateofmonth+1):
         x = str(x)
-        startdatetime = datetime.strptime( "2018/"+month+"/"+x+" 00:00:00", "%Y/%m/%d - %H:%M:%S")
-        stopdatetime = datetime.strptime( "2018/"+month+"/"+x+" 23:59:59", "%Y/%m/%d - %H:%M:%S")
+        startdatetime = datetime.strptime( "2018/"+month+"/"+x+" 04:00:00", "%Y/%m/%d %H:%M:%S")
+        stopdatetime = datetime.strptime( "2018/"+month+"/"+x+" 21:59:59", "%Y/%m/%d %H:%M:%S")
         
-        with open(path+x+"_"+month+"_"+place+".csv", "w", newline="") as f:
+        with open(path +x+"_"+month+"_2018_"+place+".csv", "w", newline="") as f:
             fw = csv.writer(f)
             fw.writerow(["Type","Start time","End time","Content Type","Content Name"])
 
             while startdatetime < stopdatetime:
 
                 for m in filename:
+                    mfile = m['file'].strip()
                     enddatetime = startdatetime + timedelta(seconds=m['duration'])
 
-                    sdatetime = datetime.strftime(startdatetime, "%Y/%m/%d - %H:%M:%S" )
-                    edatetime = datetime.strftime(enddatetime, "%Y/%m/%d - %H:%M:%S" )
-                    playtime = str(m['duration'])
+                    sdatetime = datetime.strftime(startdatetime, "%Y/%m/%d %H:%M:%S" )
+                    edatetime = datetime.strftime(enddatetime, "%Y/%m/%d %H:%M:%S" )
                     
-                    fw.writerow(["Z=Video","S="+sdatetime,"e="+edatetime,"I=video","N="+m['file']+"_V1-"+playtime+"s."+m['file']+".mp4"])
+                    fw.writerow(["Z=Video","S="+sdatetime,"E="+edatetime,"I=video","N="+mfile+".mp4"])
 
                     startdatetime = enddatetime             
 
